e0a761c8cc73a132eca3d5a3f54406b3b268bd19
AndrewNaveen <andrewnaveenkumar@gmail.com>
2018-03-11 03:42:03 -0500

    POM project initial commit

---
+++ src/com/rb/promise/pageObjectsTests/dsa.java
@@ -0,0 +1,14 @@
+package com.rb.promise.pageObjectsTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class dsa {
+ WebDriver driver=new FirefoxDriver();
+ 
+ 
+	public void sample(){
+		
+		System.out.println("execeted");
+	}
+}
